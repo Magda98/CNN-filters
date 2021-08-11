c099b9430e12dac91eb14b27881558a2fb71d2c7
Magda98 <magdakochman7@gmail.com>
2021-08-11 17:37:27 +0200

    first commit

---
+++ data.py
@@ -0,0 +1,19 @@
+import pickle
+import torch
+
+
+class Data:
+    """ class for read dataset from file"""
+
+    def __init__(self):
+        with open('cifar-10/data_batch_1', 'rb') as file:
+            data_dict = pickle.load(file, encoding='bytes')
+        self.labels = data_dict['labels'.encode()]
+        self.filenames = data_dict['filenames'.encode()]
+        self.images = data_dict['data'.encode()]
+        self.images = self.images[:200].reshape(
+            20, 10, 3, 32, 32).astype("uint8")
+        self.labels = data_dict['labels'.encode()]
+        self.labels = self.labels[:50]
+        self.images = torch.tensor(self.images)
+        self.labels = torch.tensor(self.labels)
---
+++ main.py
@@ -0,0 +1,9 @@
+import torch
+from data import Data
+
+
+if __name__ == "__main__":
+
+    torch.cuda.empty_cache()
+
+    dataset = Data()
