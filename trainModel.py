6e389f0abe4abc0f6f32a4a2a0edc13b328d13c7
Magda98 <magdakochman7@gmail.com>
2022-02-18 08:31:22 +0100

    🎨 change filenames

--- trainModel.py
+++ trainModel.py
@@ -7,7 +7,7 @@ from pytorch_model_summary import summary
 from utils import weights_init, adaptive_leraning_rate, test
 
 
-class TrainModel():
+class TrainModel:
     """
     Main class for training a model for Intel Dataset and Cifar-10
     @ dataset - dataset loader object
@@ -16,20 +16,53 @@ class TrainModel():
     @ input_size - size of image
     """
 
-    def __init__(self, dataset, method, input_size, c_kernels=[7, 5], out_channels=[30, 16], in_channels=[3, 30], p_kernel=[2, 2], p_stride=[2, 2], apt=0, dataset_name="intel", epoch=200):
+    def __init__(
+        self,
+        dataset,
+        method,
+        input_size,
+        c_kernels=[7, 5],
+        out_channels=[30, 16],
+        in_channels=[3, 30],
+        p_kernel=[2, 2],
+        p_stride=[2, 2],
+        apt=0,
+        dataset_name="intel",
+        epoch=200,
+    ):
         print("class num: {}".format(len(dataset.classes)))
         self.dataset = dataset
 
         if dataset_name == "intel":
-            self.cnn_model = CnnNet(input_size, len(dataset.classes),  c_kernels=c_kernels, out_channels=out_channels,
-                                    in_channels=in_channels, p_kernel=p_kernel, p_stride=p_stride)
+            self.cnn_model = CnnNet(
+                input_size,
+                len(dataset.classes),
+                c_kernels=c_kernels,
+                out_channels=out_channels,
+                in_channels=in_channels,
+                p_kernel=p_kernel,
+                p_stride=p_stride,
+            )
 
         elif dataset_name == "cifar":
-            self.cnn_model = CnnNetC(input_size, len(dataset.classes),  c_kernels=c_kernels, out_channels=out_channels,
-                                     in_channels=in_channels, p_kernel=p_kernel, p_stride=p_stride)
+            self.cnn_model = CnnNetC(
+                input_size,
+                len(dataset.classes),
+                c_kernels=c_kernels,
+                out_channels=out_channels,
+                in_channels=in_channels,
+                p_kernel=p_kernel,
+                p_stride=p_stride,
+            )
 
         # print model summary
-        print(summary(self.cnn_model, torch.zeros((1, 3, input_size, input_size)), show_input=False))
+        print(
+            summary(
+                self.cnn_model,
+                torch.zeros((1, 3, input_size, input_size)),
+                show_input=False,
+            )
+        )
 
         self.epoch = epoch
 
@@ -40,7 +73,7 @@ class TrainModel():
         self.dataset_name = dataset_name
 
         self.criterion = nn.NLLLoss()
-        self.lr = 0.0001
+        self.lr = 0.001
         # self.lr = 0.00001
 
         if torch.cuda.is_available():
@@ -51,7 +84,9 @@ class TrainModel():
             torch.device("cpu")
             print("GPU not available, CPU used")
 
-        self.optimizer = torch.optim.SGD(self.cnn_model.parameters(), lr=self.lr,  momentum=0.9)
+        self.optimizer = torch.optim.SGD(
+            self.cnn_model.parameters(), lr=self.lr, momentum=0.9
+        )
         # self.optimizer = torch.optim.AdamW(self.cnn_model.parameters(), lr=self.lr)
 
     def training(self):
@@ -61,7 +96,7 @@ class TrainModel():
         for e in range(self.epoch):
             self.old_param = self.cnn_model.parameters
             loss_train_temp = 0
-           # pass through all data
+            # pass through all data
             for data, exp in self.dataset.trainloader:
                 exp = exp.cuda()
                 # pass data to cuda
@@ -76,7 +111,9 @@ class TrainModel():
 
             loss_train.append(loss_train_temp)
 
-            self.cnn_model, self.optimizer = adaptive_leraning_rate(loss_train[-1], self.optimizer, self.cnn_model, self.old_param, old_loss)
+            self.cnn_model, self.optimizer = adaptive_leraning_rate(
+                loss_train[-1], self.optimizer, self.cnn_model, self.old_param, old_loss
+            )
             old_loss = loss_train[-1]
 
             # test
@@ -84,8 +121,8 @@ class TrainModel():
             loss_test.append(loss_test_temp)
 
             print("pk: {:.2f} %".format(acc))
-            print("Learning rate: {:.10f}".format(self.optimizer.param_groups[0]['lr']))
-            print('Epoch: {}.............'.format(e), end=' ')
+            print("Learning rate: {:.10f}".format(self.optimizer.param_groups[0]["lr"]))
+            print("Epoch: {}.............".format(e), end=" ")
             print("Loss: {:.4f}".format(loss))
 
         return (loss_train, loss_test, acc, self.epoch)
