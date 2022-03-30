d3017b1e62c532ad3b0084b1502d33c2afcf47f0
Magda98 <magdakochman7@gmail.com>
2022-03-30 17:31:53 +0200

    ✨ kaming experiment

--- trainModel.py
+++ trainModel.py
@@ -92,10 +92,10 @@ class TrainModel:
             torch.device("cpu")
             print("GPU not available, CPU used")
 
-        # self.optimizer = torch.optim.SGD(
-        #     self.cnn_model.parameters(), lr=self.lr, momentum=0.9
-        # )
-        self.optimizer = torch.optim.AdamW(self.cnn_model.parameters(), lr=self.lr)
+        self.optimizer = torch.optim.SGD(
+            self.cnn_model.parameters(), lr=self.lr, momentum=0.9
+        )
+        # self.optimizer = torch.optim.AdamW(self.cnn_model.parameters(), lr=self.lr)
 
     def training(self):
         loss_train = []
